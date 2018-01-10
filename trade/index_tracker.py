eb991036cb7bf5148a807a687e5b3a0db907d8e7
Nick <nick.morley@hotmail.com>
2018-01-10 16:50:27 +0000

    trade/index_tracker.py working to level of calculating reallocations and generating the list of trades required to achieve them

--- trade/index_tracker.py
+++ trade/index_tracker.py
@@ -1,18 +1,191 @@
-from bots import CoreBot
+import os
+import sys
+from pathlib import Path
+import pickle
+from pprint import pformat
+# from collections import OrderedDict
 from coinmarketcap import Market
+from cryptocompy import price
+import ccxt
+import yaml
+import numpy as np
+from bots import CoreBot
+import util
+import helpers
+import optimise
+
+root = Path(os.path.dirname(os.path.realpath(__file__))).parents[0]
+sys.path.append(str(root))
+with open(os.path.join(root, 'trade/api_keys.yaml')) as f:
+    API_KEYS = yaml.load(f)
+
+with open(os.path.join(root, 'trade/coin_thesaurus.yaml')) as f:
+    coin_thesaurus = yaml.load(f)
+
+CLIENTS = {
+    # 'GDAX': ccxt.gdax(API_KEYS['GDAX']),
+    'BINANCE': ccxt.binance(API_KEYS['BINANCE'])
+}
+
 
 class IndexTracker(CoreBot):
-    ''' Each step, rebalance portfolio based on observables and weighting dict '''
+    ''' Each step, rebalance portfolio based on weighting dict '''
 
-    def get_coin_data(self, n_coins):
-        coinmarketcap = Market()
-        coin_data = coinmarketcap.ticker(limit=n_coins, convert='EUR')
-        return coin_data
+    def rebalance(self, coin_data_list, metric_weights, coin_weight_coeffs, n_coins):
+        self.client = ccxt.binance(API_KEYS['BINANCE'])
+        self.log('Getting pair data list...')
+        pair_data_list = util.get_pair_data_list(self.client, use_checkpoint=True)
+        exchange_syms = util.symbols_in_pair_data_list(pair_data_list)
+        self.log('Getting coin market cap data...')
+        coin_data_list = self.memory[coin_data_list][-1]
+        coin_data_list = helpers.recursive_replace(coin_data_list, coin_thesaurus)  # Standardise symbol names
+        coin_data_list = [el for el in coin_data_list if el['symbol'] in exchange_syms]
 
-    def calc_weightings(self, coin_data_list, weighting_dict):
+        self.log('calculating desired spread...')
+        sym2weights = self.apply_metric_weights(coin_data_list, metric_weights)
+        sym2weights = self.apply_coin_coeffs(sym2weights, coin_weight_coeffs)
+        sym2weights = OrderedDict(sorted(sym2weights.items(), key=lambda x: x[1], reverse=True))
+        keep_syms = list(sym2weights)[:n_coins]
+        sym2weights = {sym: sym2weights[sym] for sym in keep_syms}
+        sym2weights = self.scale_weights_to_sum_1(sym2weights)
+        desired_spread = {sym: round(sym2weights[sym], 3) for sym in sym2weights}
+
+        self.log('getting current positions...')
+        positions = self.get_current_positions()
+        positions.update({sym: 0 for sym in list(set(desired_spread.keys()) - set(positions.keys()))})  # Add null vals for coins not owned
+        positions_eur = self.positions_to_values(positions)
+        current_spread = self.positions_to_spread(positions_eur)
+        current_sum_eur = sum(positions_eur.values())
+        desired_val_eur = self.spread_to_values(desired_spread, current_sum_eur)
+
+        self.log('current positions (coin amounts)', positions)
+        self.log('current positions (in EUR)', positions_eur)
+        self.log('current total value (in EUR):', current_sum_eur)
+        self.log('current spread:', current_spread)
+        self.log('desired positions (in EUR):', desired_val_eur)
+        self.log('desired spread:', self.sort_dict_desc(desired_spread))
+
+        trades_to_make = self.trades_to_make(positions, positions_eur, desired_val_eur, current_spread, desired_spread, pair_data_list)
+        # self.log('trades to make', trades_to_make)
+        self.log(str(len(trades_to_make)) + ' trades')
+        results = self.execute_trades(trades_to_make)
+
+    def trades_to_make(self, positions, positions_eur, desired_val_eur, current_spread, desired_spread, pair_data_list,
+        fee_coeff=1-0.03, amounts_to_lots=False, min_trade_eur=1):
+
+        trades_to_make = []
+
+        ''' Which coins to buy or sell '''
+        scale_factors = self.scale_old_spread_to_new(current_spread, desired_spread)
+        coins_to_reduce = [sym for sym, factor in scale_factors.items() if factor < 1]
+        coins_to_abolish = list(set(positions.keys()) - set(desired_spread.keys()))
+        coins_to_increase = list(set(desired_spread) - set(coins_to_reduce))
+        self.log('coins to reduce:', coins_to_reduce)
+        self.log('coins to abolish:', coins_to_abolish)
+        self.log('coins to buy:', coins_to_increase)
+
+        ''' Excess to reallocate '''
+        excess = {}
+        for sym in coins_to_abolish:
+            sell_amount = positions[sym]  # All
+            excess[sym] = sell_amount
+        for sym in coins_to_reduce:
+            current_amount = positions[sym]
+            reduction_factor = scale_factors[sym]
+            keep_amount = current_amount * reduction_factor
+            sell_amount = current_amount - keep_amount
+            excess[sym] = sell_amount
+        excess = self.sort_dict_desc(excess)
+        excess_eur = self.sort_dict_desc({coin: self.coin_to_eur(coin, amount) for coin, amount in excess.items()})
+        self.log('to relinquish (in coins):', excess)
+        self.log('to relinquish (in EUR):', excess_eur)
+
+        ''' Values to buy '''
+        vals_to_buy = {coin: desired_val_eur[coin] - positions_eur[coin] for coin in desired_val_eur}
+        vals_to_buy = {coin: val for coin, val in vals_to_buy.items() if val > min_trade_eur}
+        vals_to_buy = self.sort_dict_desc(vals_to_buy)
+        coin_to_buy = {coin: self.eur_to_coin(val, coin) for coin, val in vals_to_buy.items()}
+        self.log('to buy (coin amounts)', coin_to_buy)
+        self.log('to buy (in EUR)', vals_to_buy)
+
+        ''' Reallocate excess '''
+        self.log('calculating trades to make...')
+        for tcurr, val_to_buy in vals_to_buy.items():
+            excess_eur = self.sort_dict_desc(excess_eur)  # Trade most abundant coins first. Refresh for each tcurr.
+            self.log('funds to reallocate (EUR)', excess_eur)
+            for fcurr, funds_eur in excess_eur.items():
+                """
+                For each fcurr for which we have excess to reallocate, check if we still have more tcurr to buy,
+                if so, trade fcurr for tcurr via the shortest route until val_to_buy is below 1 euro.
+                """
+                if val_to_buy > min_trade_eur and funds_eur > min_trade_eur:
+                    self.log('buy ' + str(val_to_buy) + ' EUR worth of ' + tcurr + ' with ' + fcurr)
+                    if funds_eur >= val_to_buy:
+                        spend_eur = val_to_buy  # Put in much as needed
+                    else:
+                        spend_eur = funds_eur  # Put all in
+
+                    route = optimise.shortest_path(optimise.find_trade_paths(fcurr, tcurr, pair_data_list))
+                    if not route:
+                        self.log('No route from ' + fcurr + ' to ' + tcurr)
+                        continue
+
+                    spend_amount = self.eur_to_coin(spend_eur, fcurr)
+                    trade_outcomes = optimise.trade_path_outcomes(route, in_amount=spend_amount, fee_perc=0.005)
+
+                    in_amount = spend_amount
+                    for r, tcurr_amount in zip(route, trade_outcomes):
+                        trade = {
+                            'type': r['direction'],
+                            'symbol': r['symbol'],
+                            'fcurr': r['tsym'],
+                            'tcurr': r['fsym'],
+                            'fcurr_amount': in_amount,
+                            'tcurr_amount': tcurr_amount
+                        }
+                        trades_to_make.append(trade)
+                        in_amount = tcurr_amount  # Last out_amount is the next in_amount
+
+                    ''' Update values after the trade '''
+                    excess[fcurr] -= spend_amount
+                    excess_eur[fcurr] -= spend_eur
+                    val_to_buy -= spend_eur
+        return trades_to_make
+
+    def execute_trades(self, trades_to_make):
+        results = []
+        for trade in trades_to_make:
+            fcurr = trade['fcurr']
+            tcurr = trade['tcurr']
+            symbol = trade['symbol']
+            in_amount = trade['fcurr_amount']
+            out_amount = trade['tcurr_amount']
+            msg = 'Attempting to trade {fcurr_amount} {fcurr} for {tcurr_amount} {tcurr}' .format(**trade)
+            if not self.query_yes_or_no(msg):
+                quit()
+            if trade['type'] == 'buy':
+                lots = self.client.amount_to_lots(symbol, out_amount)
+            #     try:
+            #         result = self.client.create_market_buy_order(symbol, lots)
+            #         print(result)
+            #         results.append(result)
+            #     except Exception as e:  # Unacceptable lot size
+            #         raise e
+            elif trade['type'] == 'sell':
+                lots = self.client.amount_to_lots(symbol, in_amount)
+            #     try:
+            #         result = self.client.create_market_sell_order(symbol, lots)
+            #         print(result)
+            #         results.append(result)
+            #     except Exception as e:  # Unacceptable lot size
+            #         raise e
+        # print(results)
+        # return results
+
+    def apply_metric_weights(self, coin_data_list, metric_weights):
         symbols = [dd['symbol'] for dd in coin_data_list]
-        ''' Get relevant values for calculating weights '''
-        keys_of_interest = weighting_dict.keys()
+        ''' Get relevant values from coin_data_list '''
+        keys_of_interest = metric_weights.keys()
         sym2vals = {sym: {} for sym in symbols}
         for dd in coin_data_list:
             symbol = dd['symbol']
@@ -26,24 +199,103 @@ class IndexTracker(CoreBot):
             for key in keys_of_interest:
                 prop = vals[key] / totals[key]
                 sym2props[sym][key] = prop
-        ''' Integrate proportions using weighting_dict '''
+        ''' Integrate proportions using metric_weights '''
         sym2weights = {sym: {} for sym in symbols}
         for sym, props in sym2props.items():
-            for key, coeff in weighting_dict.items():
+            for key, coeff in metric_weights.items():
                 weight = props[key] * coeff
-                sym2weights[sym] = round(weight, 3)
-        # assert sum(sym2weights.values()) == 1, 'Weights do not sum to 1'
+                sym2weights[sym] = weight
+        assert round(sum(sym2weights.values())) == 1, 'Weights do not sum to 1'
         return sym2weights
 
-    def rebalance(self, coin_data_list, weighting_dict):
-        coin_data_list = self.memory[coin_data_list][-1]
-        sym2weights = self.calc_weightings(coin_data_list, weighting_dict)
-        print(sym2weights)
-        ''' Compare new weights to current positions, decide what buys and sells should be made, taking the best available route '''
+    def apply_coin_coeffs(self, sym2weights, coin_weight_coeffs):
+        for coin, coeff in coin_weight_coeffs.items():
+            try:
+                new_weight = sym2weights[coin] * coeff
+                sym2weights[coin] = new_weight
+            except KeyError as e:  # Coin not in exchange so not in sym2weights
+                pass
+        return sym2weights
+
+    ''' Data retrieval'''
+
+    def get_coin_data(self):
+        coinmarketcap = Market()
+        coin_data = coinmarketcap.ticker(limit=200, convert='EUR')
+        coin2eur = {d['symbol']: d['price_eur'] for d in coin_data}
+        coin2eur = helpers.recursive_replace(coin2eur, coin_thesaurus)
+        self.coin2eur = self.sort_dict_desc(coin2eur)
+        return coin_data
+
+    def get_current_positions(self):
+        positions = self.client.fetch_balance()['total']
+        positions = {sym: amount for sym, amount in positions.items() if amount > 0}
+        positions = helpers.recursive_replace(positions, coin_thesaurus)
+        positions = self.sort_dict_desc(positions)
+        self.positions.append(positions)
+        return positions
+
+    ''' Convenience functions '''
+
+    def positions_to_values(self, positions):
+        values = {sym: self.coin_to_eur(sym, amount) for sym, amount in positions.items()}
+        values = self.sort_dict_desc(values)
+        return values
+
+    def positions_to_spread(self, current_values):
+        _sum = sum(current_values.values())
+        spread = {sym: amount / _sum for sym, amount in current_values.items()}
+        spread = self.sort_dict_desc(spread)
+        return spread
+
+    def spread_to_values(self, spread, current_eur_tot):
+        values = {sym: current_eur_tot * prop for sym, prop in spread.items()}
+        values = self.sort_dict_desc(values)
+        return values
+
+    def scale_old_spread_to_new(self, current_spread, desired_spread):
+        ''' Find changes to make to current positions '''
+        scale_factors = {}
+        for sym, weight in desired_spread.items():
+            try:
+                current_prop = current_spread[sym]
+                factor = weight / current_prop
+                scale_factors[sym] = factor
+            except ZeroDivisionError as e:  # Current position is zero
+                scale_factors[sym] = np.nan
+        scale_factors = self.sort_dict_desc(scale_factors)
+        return scale_factors
+
+    def scale_weights_to_sum_1(self, sym2weights):
+        weights = sym2weights.values()
+        _sum = sum(weights)
+        factor = 1 / _sum
+        sym2weights = {sym: sym2weights[sym] * factor for sym in sym2weights}
+        assert round(sum(sym2weights.values())) == 1, 'Weights do not sum to 1'
+        return sym2weights
+
+    def coin_to_eur(self, coin, amount):
+        price_eur = self.coin2eur[coin]
+        return float(amount) * float(price_eur)
+
+    def eur_to_coin(self, eur_amount, coin):
+        price_eur = self.coin2eur[coin]
+        return float(eur_amount) / float(price_eur)
+
+    def sort_dict_desc(self, _dict):
+        return OrderedDict(sorted(_dict.items(), key=lambda x: x[1], reverse=True))
+
+    def query_yes_or_no(self, question):
+        ''' Check user confirmation for testing purposes '''
+        print(question)
+        choice = input()
+        if choice == 'y':
+            return True
+        return False
 
 
 if __name__ == '__main__':
 
-    bot = IndexTracker(funds=1000)
+    bot = IndexTracker()
     bot.load_rule_set('index_tracker.yaml')
     bot.step()
--- trade/util.py
+++ trade/util.py
@@ -1,47 +1,42 @@
+import os
+from pathlib import Path
+import pickle
 import ccxt
-from forex_python.converter import CurrencyRates
+# from forex_python.converter import CurrencyRates
 import pandas as pd
 from cryptocompy import price
+import yaml
 
 FIAT = ['GBP', 'USD', 'EUR']
 
+CWD = os.path.dirname(os.path.realpath(__file__))
+ROOT = Path(CWD).parents[0]
+with open(os.path.join(ROOT, 'trade/api_keys.yaml')) as f:
+    API_KEYS = yaml.load(f)
 
-def get_total_balance(clients, gbp_only=False, wallets=None, funds_invested=None):
-    """
-    Analyses balance of funds across one or more exchanges.
-    :param clients: Dict of ccxt authenticated clients with exchange names as keys
-    :param wallets: optional dict of coins held in private wallets (e.g. {'BTC': 1.1, 'LTC': 0.02})
-    :param gbp_only: optionally return GBP values only
-    :param funds_invested: optionally input GBP invested for profit calculation
-    :return: DataFrame of balance
-    """
+CLIENTS = {
+    'BINANCE': ccxt.binance(API_KEYS['BINANCE']),
+    # 'GDAX': ccxt.gdax(API_KEYS['GDAX'])
+}
+
+
+def get_total_balance(clients, gbp_only=False):
+    """Returns df of total balance over all exchanges"""
     eur2gbp = CurrencyRates().get_rate('EUR', 'GBP')
-    pd.set_option('expand_frame_repr', False)
-    pd.options.display.float_format = '{:.2f}'.format
-
 
     # build df columns from currencies
     df_cols = ['Exchange']
     totals = {}
     coins = set()
     for ex in clients:
-        totals_raw = clients[ex].fetch_balance()['total']
-        totals[ex] = {}
-        for curr in totals_raw:
-            if totals_raw[curr] > 0.:
-                totals[ex][curr] = totals_raw[curr]
+        totals[ex] = clients[ex].fetch_balance()['total']
         for curr in totals[ex]:
             if curr not in FIAT:
                 coins.add(curr)
-    # add wallets
-    totals['Wallets'] = {}
-    for curr in wallets:
-        totals['Wallets'][curr] = wallets[curr]
-        coins.add(curr)
-
     df_cols += list(coins) + ['EUR', 'GBP', 'Total (GBP)']
     df = pd.DataFrame(columns=df_cols)
     avg_prices = price.get_current_price(list(coins), 'EUR')
+    print(df_cols)
 
     # build row values for each exchange
     for ex in totals:
@@ -74,10 +69,12 @@ def get_total_balance(clients, gbp_only=False, wallets=None, funds_invested=None
             row_totals[col] = sum(df[col].values)
             if col == 'EUR':
                 row_gbp_totals[col] = row_totals[col] * eur2gbp
+                print(eur2gbp)
             elif col == 'GBP' or col == 'Total (GBP)':
                 row_gbp_totals[col] = row_totals[col]
             else:
                 row_gbp_totals[col] = row_totals[col] * avg_prices[col]['EUR'] * eur2gbp
+    print(row_gbp_totals)
     df = df.append(row_totals, ignore_index=True)
     df = df.append(row_gbp_totals, ignore_index=True)
 
@@ -93,27 +90,13 @@ def get_total_balance(clients, gbp_only=False, wallets=None, funds_invested=None
     perc_row = {}
     for col in df_cols:
         if col == 'Exchange':
-            perc_row[col] = '%'
+            perc_row[col] = 'Total %'
         else:
             perc_row[col] = 100 * df[col].values[-1] / df['Total (GBP)'].values[-1]
     perc_row['%'] = 100
     df = df.append(perc_row, ignore_index=True)
 
-    # formatting and sorting
-    df = df.loc[:, (df > 1).any(axis=0)]  # delete columns with less than £1 in
-    df = df[:-3].sort_values(by='%', ascending=False).reset_index(drop=True).append(df[-3:], ignore_index=True)
-    df = pd.concat(
-        [df.iloc[:, 0], df.iloc[:, 1:-3].sort_values(by=len(df) - 1, ascending=False, axis=1), df.iloc[:, -3:]], axis=1)
-
-    print(df)
-
-    # profit calc
-    if funds_invested is not None:
-        current_value = df['Total (GBP)'].values[-2]
-        profit = current_value - funds_invested
-        profitperc = 100 * profit/funds_invested
-        print('\nInvested: £{:.2f}, Current Value: £{:.2f}, Profit: £{:.2f}, {:+.2f}%'.format(
-                funds_invested, current_value, profit, profitperc))
+    df = df.loc[:, (df != 0).any(axis=0)]  # delete columns where everything is 0
 
     if gbp_only:
         return df[['Exchange', 'Total (GBP)']][:-2]
@@ -121,5 +104,51 @@ def get_total_balance(clients, gbp_only=False, wallets=None, funds_invested=None
         return df
 
 
+def get_pair_data_list(client, use_checkpoint=False):
+    checkpoint_path = os.path.join(CWD, 'checkpoints/pair_data_list_checkpoint.p')
+    if use_checkpoint:
+        with open(checkpoint_path, 'rb') as f:
+            return pickle.load(f)
+    markets = client.load_markets()
+    pair_data_list = []
+    for symbol, dd in markets.items():
+        ticker = client.fetch_ticker(symbol)
+        pair_data_list.append({
+            'fsym': dd['base'],
+            'tsym': dd['quote'],
+            'price': ticker['last'],
+            'symbol': symbol,
+            'direction': 'buy',
+            'exchange': client.name
+        })
+    pair_data_list = get_inv_pairs(pair_data_list)
+    with open(checkpoint_path, 'wb') as f:
+        pickle.dump(pair_data_list, f)
+    return pair_data_list
+
+
+def symbols_in_pair_data_list(pair_data_list):
+    return list(set([sublist for _list in [[d['tsym'], d['fsym']] for d in pair_data_list] for sublist in _list]))
+
+
+def get_inv_pairs(pair_data_list):
+    pairs_inv = []
+    for pair in pair_data_list:
+        pair_inv = pair.copy()
+        try:
+            pair_inv['price'] = 1 / pair['price']
+            pair_inv['fsym'] = pair['tsym']
+            pair_inv['tsym'] = pair['fsym']
+            pair_inv['direction'] = 'sell'
+            pairs_inv.append(pair_inv)
+        except ZeroDivisionError as e:
+            pass
+    pair_data_list = pair_data_list + pairs_inv
+    return pair_data_list
+
+
 if __name__ == '__main__':
-    pass
+    print(get_total_balance(CLIENTS, gbp_only=False))
+
+
+
