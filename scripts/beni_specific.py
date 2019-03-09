d74e3f383f1523f4916be684b16ea82eba8ac25c
Nick Morley <nick.morley111@gmail.com>
2019-03-09 12:42:00 +0100

    .

--- scripts/beni_specific.py
+++
@@ -1,7 +0,0 @@
-from trade.util import get_total_balance
-from private import CLIENTS  # Dict of ccxt clients instantiated with private API keys
-
-
-if __name__ == '__main__':
-    df = get_total_balance(CLIENTS, wallets={'SC': 11740, 'XRB': 110.8}, gbp_only=False, funds_invested=23500)
-
